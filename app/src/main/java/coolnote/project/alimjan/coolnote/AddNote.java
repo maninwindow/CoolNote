6ff3e2300a07e037c977ae5362cff9bd66059ca9
Alimjan <maninwindow@hotmail.com>
2016-05-14 16:39:22 +0800

    Developed new functions such as: page intent

---
+++ app/src/main/java/coolnote/project/alimjan/coolnote/AddNote.java
@@ -0,0 +1,18 @@
+package coolnote.project.alimjan.coolnote;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+/**
+ * Created by alimjan on 5/14/16.
+ */
+public class AddNote extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.add_note);
+    }
+
+}
